a1a5cf6997a6946dab3809fbaa3304d4d4f3f8ae
sandeep <sandeep.shukla@wynk.in>
2025-01-01 07:39:51 +0530

    commit 1.1.2
    commit 1.1.2

--- src/pages/GPG.tsx
+++ src/pages/GPG.tsx
@@ -6,9 +6,9 @@ import Link from "next/link";
 
 const GPG = () => {
   return (
-    <div className="relative h-screen">
+    <div className="relative h-screen overflow-hidden">
       <Image className="w-4/5 mx-auto" alt="Welcome Hero" src={welcomeHero} />
-      <div className="text-center absolute bottom-0 w-full h-[80%]">
+      <div className="text-center absolute bottom-0 w-full max-h-[100%]">
         <Image
           className="relative h-full"
           alt="Welcome Banner"
@@ -19,7 +19,7 @@ const GPG = () => {
           className="absolute transform top-64 left-1/2 
             -translate-x-1/2 py-2 px-5 text-stone-800 font-medium 
             text-xl text-center rounded-md shadow-lg mx-auto bg-white
-            flex items-center gap-2 hover:opacity-85"
+            flex items-center gap-2 hover:opacity-85 border border-brand"
         >
           Get Started <FaArrowRight />
         </Link>
